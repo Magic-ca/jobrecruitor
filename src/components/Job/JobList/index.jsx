fd752a633a07e942a757f79673461168344d98bf
Magic0913 <magicjohnson.ca@gmail.com>
2022-07-18 18:12:37 +0900

    fix useReducer error

--- src/components/Job/JobList/index.jsx
+++ src/components/Job/JobList/index.jsx
@@ -3,7 +3,6 @@ import { Table } from "react-bootstrap";
 import JobItem from "../JobItem";
 
 const JobList = ({ jobs }) => {
-  console.log(jobs);
   return (
     <Table className="table-sort table-arrows remember-sort mt-5">
       <thead>
--- src/pages/job/index.jsx
+++ src/pages/job/index.jsx
@@ -1,5 +1,5 @@
 import { createContext, useReducer } from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import JobCreator from '../../components/Job/JobCreator';
 import JobList from '../../components/Job/JobList';
@@ -8,17 +8,13 @@ import { jobReducer, initialState } from "../../store/JobStore";
 const JobContext = createContext();
 
 const Job = () => {
-  const [jobNextId, setJobNextId] = useState(1);
-  const [{ jobs }, dispatch] = useReducer(jobReducer, initialState);
+  const [{ jobNextId, jobs }, dispatch] = useReducer(jobReducer, initialState);
 
   useEffect(() => {
     const jobs = JSON.parse(localStorage.getItem('jobs'));
     const jobNextId = JSON.parse(localStorage.getItem('jobNextId'));
-    if (jobNextId) {
-      setJobNextId(jobNextId);
-    }
     if (jobs && jobs.length > 0) {
-      dispatch({ type: "set", jobs: jobs });
+      dispatch({ type: "set", jobs: jobs, jobNextId: jobNextId });
     }
   }, []);
 
@@ -31,7 +27,7 @@ const Job = () => {
     <JobContext.Provider value={{ dispatch: dispatch }}>
       <Container>
         <h1 className='mt-4'>Job Manager</h1>
-        <JobCreator jobNextId={jobNextId} setJobNextId={setJobNextId} />
+        <JobCreator jobNextId={jobNextId} />
         <JobList jobs={jobs} />
       </Container>
     </JobContext.Provider>
